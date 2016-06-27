071dc944d6cf4cde279350d350d06f15939147c7
Alan's Computer <Alan's Computer>
2016-06-27 13:47:53 -0400

    uploaded lesson 2 project

---
+++ CourtCounter/src/com/example/courtcounter/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.courtcounter;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+	
+	int scoreTeamA = 0;
+	int scoreTeamB = 0;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	  /**
+	   * Displays the given score for Team A.
+	   */
+	  public void displayForTeamA(int score) {
+	    TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+	    scoreView.setText(String.valueOf(score));
+	  }
+	  public void addThreeForTeamA(View v){
+		  scoreTeamA += 3;
+		  displayForTeamA(scoreTeamA);
+	  }
+	  public void addTwoForTeamA(View v){
+		  scoreTeamA += 2;
+		  displayForTeamA(scoreTeamA);
+	  }
+	  public void addOneForTeamA(View v){
+		  scoreTeamA += 1;
+		  displayForTeamA(scoreTeamA);
+	  }
+	  
+	  /**
+	   * Displays the given score for Team B.
+	   */
+	  public void displayForTeamB(int score) {
+	    TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+	    scoreView.setText(String.valueOf(score));
+	  }
+	  public void addThreeForTeamB(View v){
+		  scoreTeamB += 3;
+		  displayForTeamB(scoreTeamB);
+	  }
+	  public void addTwoForTeamB(View v){
+		  scoreTeamB += 2;
+		  displayForTeamB(scoreTeamB);
+	  }
+	  public void addOneForTeamB(View v){
+		  scoreTeamB += 1;
+		  displayForTeamB(scoreTeamB);
+	  }
+	  
+	  public void resetScore(View v){
+		  scoreTeamA = 0;
+		  scoreTeamB = 0;
+		  displayForTeamA(scoreTeamA);
+		  displayForTeamB(scoreTeamB);
+	  }
+}
